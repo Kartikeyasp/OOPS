cbb0bd79b36cb93f77860af52036be3eb65fb5c1
Kartikeya Parihar <kartikeyaparihar22@gmail.com>
2025-09-15 19:23:04 +0530

    add sum documentation

--- sum.h
+++ sum.h
@@ -3,8 +3,13 @@
 #include "succ.h"
 #include "pred.h"
 
+//Objective: Find the sum of two non negative numbers.
+//Input: Two non negative numbers x(size_t) y(size_t)
+//Returns: Sum of two non negative numbers 
 size_t sum(size_t x, size_t y)
 {
+    //Base Case: y==0
+    //Approach: Recursively call sum function with successor of x and predecessor of y till base case is reached
     if(y==0)
     {
         return x;
